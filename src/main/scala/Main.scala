7081c64a65b4a218dfcc4972a27c4f416d29cbdc
sabasaikyouno <0852kuro@gmail.com>
2023-10-26 23:23:22 +0900

    Main実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,32 @@
+import LoadedTweets.{loadedList, loadedWrite}
 import TwitterFollowing.followingList
 import TwitterGetTweet.tweetList
 import TwitterLogin.login
 import org.openqa.selenium.chrome.ChromeDriver
+import TwitterActions._
+
+import scala.collection.mutable
 
 object Main {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     implicit val chrome: ChromeDriver = new ChromeDriver()
+
+    val loadedHashSet = mutable.HashSet(loadedList: _*)
+
+    scala.sys.addShutdownHook {
+      loadedWrite(loadedHashSet)
+    }
 
     val twitterId = "@forsuger169566"
     login(twitterId, "liens1234")
 
-    val f = followingList(twitterId)
+    followingList(twitterId)
       .flatMap(following => tweetList(following))
-
-    println(f)
+      .filterNot(tweetUrl => loadedHashSet.contains(tweetUrl))
+      .foreach { tweetUrl =>
+        likeAndRepost(tweetUrl)
+        loadedHashSet += tweetUrl
+      }
   }
 }
