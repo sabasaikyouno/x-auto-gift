8cb9d692c4e922e1f500052b4848a688d6296827
sabasaikyouno <0852kuro@gmail.com>
2023-10-26 22:24:17 +0900

    getTweet実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,19 @@
+import TwitterFollowing.followingList
+import TwitterGetTweet.tweetList
+import TwitterLogin.login
+import org.openqa.selenium.chrome.ChromeDriver
+
 object Main {
 
+  def main(args: Array[String]) = {
+    implicit val chrome: ChromeDriver = new ChromeDriver()
+
+    val twitterId = "@forsuger169566"
+    login(twitterId, "liens1234")
+
+    val f = followingList(twitterId)
+      .flatMap(following => tweetList(following))
+
+    println(f)
+  }
 }
--- src/main/scala/TwitterGetTweet.scala
+++ src/main/scala/TwitterGetTweet.scala
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
+import org.joda.time.{DateTime, Days}
 
 import scala.jdk.CollectionConverters._
 
@@ -9,21 +10,21 @@ object TwitterGetTweet {
     chrome.get(url)
     Thread.sleep(5000)
 
-    //ツイートurl一覧の要素取得
-    val tweetEle = chrome
+    //ツイートを取得し、１週間以内なら取得する
+    chrome
       .findElement(By.xpath("//div[contains(@aria-label, 'Timeline:')]"))
-      .findElements(By.cssSelector(".css-4rbku5.css-18t94o4.css-901oao.r-1bwzh9t.r-1loqt21.r-xoduu5.r-1q142lx.r-1w6e6rj.r-37j5jr.r-a023e6.r-16dba41.r-9aw3ui.r-rjixqe.r-bcqeeo.r-3s2u2q.r-qvutc0"))
-
-    //ツイートurl一覧要素からツイートurlを抽出
-    val t = tweetEle
+      .findElements(By.cssSelector(".css-4rbku5.css-18t94o4.css-901oao.r-14j79pv.r-1loqt21.r-xoduu5.r-1q142lx.r-1w6e6rj.r-37j5jr.r-a023e6.r-16dba41.r-9aw3ui.r-rjixqe.r-bcqeeo.r-3s2u2q.r-qvutc0"))
       .asScala
-      .map(
-        _.getAttribute("href")
-      ).toList
-
-    println(tweetEle.asScala.toList)
-    println(t)
-
-    t
+      .filter(ele => getDiffDays(ele.getAttribute("datetime")) <= 7)
+      .map(_.getAttribute("href"))
+      .toList
   }
+
+  //現在との差を日にちで表す
+  private def getDiffDays(dateStr: String) = {
+    val date = new DateTime(dateStr)
+
+    Days.daysBetween(new DateTime(), date).getDays
+  }
+
 }
