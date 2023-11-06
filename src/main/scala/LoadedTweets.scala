c2bef056b33477780ec54e11e9d06b0badd0f68d
sabasaikyouno <0852kuro@gmail.com>
2023-11-06 22:18:58 +0900

    複数chrome起動できるようにした

--- src/main/scala/LoadedTweets.scala
+++ src/main/scala/LoadedTweets.scala
@@ -6,13 +6,13 @@ import scala.collection.mutable
 
 object LoadedTweets {
   def loadedList = {
-    val reader = CSVReader.open(new File("loaded.csv"))
+    val reader = CSVReader.open(new File("src\\main\\resources\\loaded.csv"))
 
     reader.all().head
   }
 
   def loadedWrite(list: mutable.HashSet[String]) = {
-    val writer = CSVWriter.open(new File("loaded.csv"))
+    val writer = CSVWriter.open(new File("src\\main\\resources\\loaded.csv"))
 
     writer.writeRow(list.toList)
   }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 import java.io.File
+import java.time.Duration
 
 import LoadedTweets.{loadedList, loadedWrite}
 import TwitterFollowing.followingList
 import TwitterGetTweet.tweetList
 import TwitterLogin.login
-import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 import TwitterActions._
 import com.github.tototoshi.csv.CSVReader
 
@@ -13,7 +14,13 @@ import scala.collection.mutable
 object Main {
 
   def main(args: Array[String]): Unit = {
-    implicit val chrome: ChromeDriver = new ChromeDriver()
+    System.setProperty("webdriver.chrome.driver", "C:\\x-auto-gift\\chrome\\chromedriver_win32\\chromedriver.exe")
+    val options = new ChromeOptions()
+    options.addArguments("--user-data-dir=C:\\x-auto-gift\\chrome")
+    options.addArguments("--profile-directory=Profiel1")
+
+    implicit val chrome: ChromeDriver = new ChromeDriver(options)
+    chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(5))
 
     val loadedHashSet = mutable.HashSet(loadedList: _*)
 
@@ -21,7 +28,7 @@ object Main {
       loadedWrite(loadedHashSet)
     }
 
-    val twitterLoginIte = CSVReader.open(new File("twitter_login.csv")).iterator
+    val twitterLoginIte = CSVReader.open(new File("src\\main\\resources\\twitter_login.csv")).iterator
     val twitterId = twitterLoginIte.next().head
     login(twitterId, twitterLoginIte.next().head)
 
@@ -29,7 +36,6 @@ object Main {
       .flatMap(following => tweetList(following))
       .filterNot(tweetUrl => loadedHashSet.contains(tweetUrl))
       .foreach { tweetUrl =>
-        println(tweetUrl)
         likeAndRepost(tweetUrl)
         loadedHashSet += tweetUrl
       }
